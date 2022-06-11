baadf545397fef91226954a2a7a6e309286477cb
Trua Shamu <sofia_mit@hotmail.com>
2022-06-11 15:17:23 -0500

    2

---
+++ batteries/spindler_battery.py
@@ -0,0 +1,13 @@
+from abc import ABC
+from dateutil import relativedelta
+
+from batteries.battery import Battery
+
+class SpindlerBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        diff = relativedelta(self.current_date, self.last_service_date)
+        return (diff.years >= 2)
